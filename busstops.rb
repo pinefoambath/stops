6479bdec1547ce1ce5838bf65ec921aa90f85126
Andrea Kreideweiss <akreideweiss@gmail.com>
2020-12-01 16:14:56 +0100

    refactored search code

---
+++ busstops.rb
@@ -0,0 +1,27 @@
+require 'csv'
+
+
+def findstops
+    filepath = 'stops.csv'
+    stops_array = []
+
+    CSV.foreach(filepath) do |row|
+    stops_array << row[2].downcase
+    end
+
+    puts "Please enter a search term"
+    query = gets.chomp.downcase
+    output = stops_array.select { |stop| stop.start_with? "#{query}" }
+    output = output.map(&:capitalize)
+    puts "These stops match your search: #{output}"
+
+end
+
+findstops
+
+
+
+
+
+
+
